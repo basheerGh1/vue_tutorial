ae1800b2d4611537e468e81ace68ea27e40a6c95
basheerGh1 <basheer.ghawnji@timecut.se>
2024-03-04 15:55:11 +0100

    data and methods

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ new Vue({
     //connection to dev with id #vue-app
     el: "#vue-app",
     data: {
-        name: "shaun"
+        name: "shaun",
+        job: 'Ninja'
+    },
+    methods: {
+        greet: function(time){
+            //this.data.name ❌
+            //this.name 👍
+            return `Good ${time} ${this.name}`
+        }
     }
 
 })

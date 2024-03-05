9c15b10063c1005fc5d26190d98ac7e6524ca975
basheerGh1 <basheer.ghawnji@timecut.se>
2024-03-05 11:59:02 +0100

    dynamic css

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 new Vue({
   el: "#app-vue",
   data: {
-
+    available: true,
+    nearby: false
   },
   methods: {
     
+  },
+  computed: {
+    compClasses: function() {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      }
+    }
   }
 })

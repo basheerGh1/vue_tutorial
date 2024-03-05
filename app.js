c6531d3ca200626a916d2c1aecf75aaf83e376a2
basheerGh1 <basheer.ghawnji@timecut.se>
2024-03-05 10:28:10 +0100

    computed properties

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 new Vue({
   el: "#app-vue",
   data: {
-
+    age: 20,
+    a: 0,
+    b: 0
   },
   methods: {
-    
+
+  },
+  computed: {
+    addToA: function() {
+      console.log("A runs")
+      return this.a + this.age
+     },
+     addToB: function() {
+      console.log("B runs")
+       return this.b + this.age
+     },
   }
 })
+
+
+
+// methods: {
+//   addToA: function() {
+//    return this.a + this.age
+//   },
+//   addToB: function() {
+//     return this.b + this.age
+//   },
+// }
+
+// when click on add a button the two method addToA() and addToB() runs
+// to avoid this behavior we going to we computed
